455b7c11c53c10f248c871a0b92735cfcfc8f63a
Michael F <michael.foe@live.com>
2016-09-06 18:33:10 -0400

    Asteroid

---
+++ Asteroids/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+	}
+}
