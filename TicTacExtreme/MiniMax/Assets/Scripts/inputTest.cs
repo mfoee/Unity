30bc39763cda6f137d731682bef39635370dc37e
Michael F <michael.foe@live.com>
2016-09-06 23:16:34 -0400

    MiniMax

---
+++ TicTacExtreme/MiniMax/Assets/Scripts/inputTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inputTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void input()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+        //    var texture:Texture2D = Resources.Load("O.jpeg");
+        //    renderer.material.mainTexture = texture;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
